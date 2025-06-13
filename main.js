baa0008443a5d7f9f18a7d8f0482474940220836
dageta <davi.linhares.nascimento@escola.pr.gov.br>
2025-06-13 07:41:11 -0300

    .

--- main.js
+++ main.js
@@ -20,7 +20,13 @@ const loop = setInterval(() => {
 
         mario.style.animation = "none";
         mario.style.left = `${marioPosition}px`;
+
+        mario.src = ".img/game-over.png";
+        mario.style.width = "75px";
+        mario.style.marginLeft = "50px";
+
+        clearInterval(loop);
     } 
-})
+},10);
 document.addEventListener("keydown", jump);
 
